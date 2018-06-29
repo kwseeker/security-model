1fcb2361e1a9667296c8d924d6f9bcc68ed0a470
Arvin Lee <xiaohuileee@gmail.com>
2018-06-29 19:31:40 +0800

    创建UserController及测试用例

---
+++ security-demo/src/main/java/top/kwseeker/controller/UserController.java
@@ -0,0 +1,101 @@
+package top.kwseeker.controller;
+
+
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+import top.kwseeker.dto.User;
+import top.kwseeker.dto.UserQueryCondition;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@RestController //如果方法返回是一个对象或对象的容器，会自动转换成JSON的格式
+@RequestMapping("/user")
+public class UserController {
+
+    //注册用户
+    @PostMapping("/register")
+    public void register(User user, HttpServletRequest request) {
+        //以用户名作为用户唯一标识
+        String userId = user.getUsername();
+    }
+
+    //创建用户
+    @PostMapping
+    public User create(@RequestBody User user) {
+        //TODO: Id号自增
+        System.out.println(user.getId() + "\n"
+                + user.getUsername() + "\n"
+                + user.getPassword() + "\n"
+                + user.getBirthday()
+        );
+        // To do store operation
+        return user;
+    }
+
+    //查询用户列表
+    @GetMapping
+    //public List<User> query(@RequestParam String username, @PageableDefault(page = 2, size = 5, sort = "username, desc") Pageable pageable) {
+    //public List<User> query(@RequestParam(name = "username", required = false, defaultValue = "kwseeker") String un,
+    //                        @PageableDefault(page = 2, size = 5, sort = "username, desc") Pageable pageable) {
+    public List<User> query(UserQueryCondition condition, @PageableDefault(page = 2, size = 2, sort = "username, asc") Pageable pageable) {
+
+        System.out.println(ReflectionToStringBuilder.toString(condition, ToStringStyle.MULTI_LINE_STYLE));  //对象转化为String, 以多行显示
+        System.out.println("page number: " + pageable.getPageSize() + "\n"
+                + "page size: " + pageable.getPageNumber() + "\n"
+                + "page sort: " + pageable.getSort());
+
+        List<User> userList = new ArrayList<>();
+        userList.add(new User("Arvin", "123456"));
+        userList.add(new User("Bob", "123456"));
+        userList.add(new User("Candy", "123456"));
+        userList.add(new User("Danny", "123456"));
+
+        return userList;
+    }
+
+    //查询当前用户
+
+    //绑定用户
+
+    //获取指定用户的信息（通过用户id）
+    @GetMapping(value = "/{id:\\d+}")   //正则表达式约束id值为多位整数
+    public User getInfo(@PathVariable String id) {
+
+//        if( true ) {     //TODO: 查数据库判断id是否存在
+//            throw new UserNotExistException(id);
+//        }
+//        return null;
+
+        User user = new User();
+        user.setId("1");
+        user.setUsername("Arvin");
+        user.setPassword("123458");
+        user.setBirthday(new Date());
+        return user;
+    }
+
+    //更新用户信息数据
+    @PutMapping("/{id:\\d+}")
+    public User updateInfo(@RequestBody User user) {
+        System.out.println(ReflectionToStringBuilder.toString(user, ToStringStyle.MULTI_LINE_STYLE));
+        //更新用户数据的操作
+        //...
+
+        return user;
+    }
+
+    //删除用户
+    @DeleteMapping("/{id:\\d+}")
+    public void delete(@PathVariable String id) {
+        System.out.println("User id to delete: " + id);
+        //删除用户操作
+        //...
+    }
+
+}
---
+++ security-demo/src/test/java/top/kwseeker/controller/UserControllerTest.java
@@ -0,0 +1,115 @@
+package top.kwseeker.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    public void register() {
+    }
+
+    @Test
+    public void create() throws Exception {
+        Date date = new Date();
+        String content = "{\"username\":\"Eric\", \"password\":\"123456\", \"birthday\":"+date.getTime()+"}";
+        //System.out.println("new user created: " + content);
+        String result = mockMvc.perform(MockMvcRequestBuilders
+                .post("/user")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(content))
+            .andExpect(status().isOk())
+            .andReturn().getResponse().getContentAsString();
+
+        System.out.println(result);
+    }
+
+    @Test
+    public void query() throws Exception {
+        // TODO: MockMvc 使用详解
+        // 模拟发送Get请求
+        String result = mockMvc.perform(MockMvcRequestBuilders
+                .get("/user")
+//                .param("username", "Arvin")
+                .param("username", "Arvin")
+                .param("ageBegin", "18")
+                .param("ageEnd", "35")
+                .param("xxx", "yyy")
+//                .param("size", "15")    //每页15条数据
+//                .param("page", "3")     //查询从第3页开始
+//                .param("sort", "age,desc")  //查询结果以age降序排序
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(4))
+            .andReturn().getResponse().getContentAsString();  //关于 jsonPath()的使用： https://github.com/json-path/JsonPath
+
+        System.out.println(result);
+    }
+
+    @Test
+    public void getInfo() throws Exception {
+        String result = mockMvc.perform(MockMvcRequestBuilders
+                .get("/user/1")
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(status().isOk())
+            .andReturn().getResponse().getContentAsString();    //返回请求内容
+
+        System.out.println(result);
+    }
+
+    @Test
+    public void whenGetInfoFail() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/user/a")
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void updateInfo() throws Exception {
+        Date date = new Date(LocalDateTime.now().plusYears(1).atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+        String content = "{\"id\":\"1\", \"username\":\"ATooLongNameExample\",\"password\":null,\"birthday\":"+date.getTime()+"}";
+        mockMvc.perform(MockMvcRequestBuilders
+                .put("/user/1").contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(content))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.id").value("1"))
+            .andExpect(jsonPath("$.username").isString());
+    }
+
+    @Test
+    public void delete() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .delete("/user/1")
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk());
+    }
+}
