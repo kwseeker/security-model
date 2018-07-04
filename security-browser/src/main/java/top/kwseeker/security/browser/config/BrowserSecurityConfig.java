8384c6febb8fefc96b18aeca55367c26f1cfdec9
Arvin Lee <xiaohuileee@gmail.com>
2018-07-04 23:14:41 +0800

    Spring Security Remember-Me 功能实现

--- security-browser/src/main/java/top/kwseeker/security/browser/config/BrowserSecurityConfig.java
+++ security-browser/src/main/java/top/kwseeker/security/browser/config/BrowserSecurityConfig.java
@@ -1,19 +1,28 @@
 package top.kwseeker.security.browser.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import top.kwseeker.security.browser.authentication.MyAuthenticationFailureHandler;
 import top.kwseeker.security.browser.authentication.MyAuthenticationSuccessHandler;
 import top.kwseeker.security.core.properties.SecurityProperties;
 import top.kwseeker.security.core.validate.code.ValidateCodeFilter;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
     private SecurityProperties securityProperties;
@@ -23,6 +32,11 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private MyAuthenticationFailureHandler myAuthenticationFailureHandler;
 
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         ValidateCodeFilter validateCodeFilter = new ValidateCodeFilter();
@@ -30,11 +44,18 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
         validateCodeFilter.setSecurityProperties(securityProperties);
         validateCodeFilter.afterPropertiesSet();    //TODO: 不是自动执行的么？
 
+        logger.info("RememberMe Token超时时间： " + securityProperties.getBrowser().getRememberMeSeconds());
+
         http.formLogin()    //支持很多种认证方式，如：formLogin/csrf/rememberMe/anonymous/cors/httpBasic/openidLogin
                 .loginPage("/authentication/login")          //重新指定登录页面，从而取代Spring Security默认的那个简陋的页面
                 .loginProcessingUrl("/authentication/form") //指定 UsernamePasswordAuthenticationFilter 认证的请求
                 .successHandler(myAuthenticationSuccessHandler) //默认的处理器SimpleUrlAuthenticationFailureHandler是重定向跳转，而更常用的应该是返回JSON字段
                 .failureHandler(myAuthenticationFailureHandler)
+                .and()
+            .rememberMe()
+                .tokenRepository(persistentTokenRepository())   //TODO: 为什么这里还是需要调接口？
+                .tokenValiditySeconds(securityProperties.getBrowser().getRememberMeSeconds())   //过期时间
+                .userDetailsService(userDetailsService)     //指定用于查找UserDetails信息的UserDetailsService实例
                 .and()
             .authorizeRequests()
                 .antMatchers("/authentication/login",
@@ -50,5 +71,17 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    //RememberMe功能存储用户Token的Repository
+    //JdbcTokenRepositoryImpl 这个类可以根据 application.yml 中配置的数据源，自动创建存储token数据的表，
+    // 并提供初始化、创建、删除、更新 token 的方法
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        logger.info("persistentTokenRepository() 初始化");
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
+//        tokenRepository.setCreateTableOnStartup(true);  //建表应该放在项目部署的脚本里面
+        return tokenRepository;
     }
 }
--- security-core/src/main/java/top/kwseeker/security/core/properties/BrowserProperties.java
+++ security-core/src/main/java/top/kwseeker/security/core/properties/BrowserProperties.java
@@ -4,6 +4,7 @@ public class BrowserProperties {
 
     private String loginPage = "/login-std.html";
     private LoginType loginType = LoginType.JSON;
+    private int rememberMeSeconds = 3600*24*10;   //一般都是10天左右
 
     public String getLoginPage() {
         return loginPage;
@@ -19,5 +20,13 @@ public class BrowserProperties {
 
     public void setLoginType(LoginType loginType) {
         this.loginType = loginType;
+    }
+
+    public int getRememberMeSeconds() {
+        return rememberMeSeconds;
+    }
+
+    public void setRememberMeSeconds(int rememberMeSeconds) {
+        this.rememberMeSeconds = rememberMeSeconds;
     }
 }
